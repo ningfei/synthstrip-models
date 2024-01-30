1248a8f0d6b0f1436cdeffc7760e46c9e0a63078
Ningfei Li <ningfei.li@gmail.com>
2024-01-30 14:59:35 +0100

    Initial repository.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+# Your package details
+setup(
+    name='synthstrip-models',
+    version='1.0',
+    description='',
+    author='Ningfei Li',
+    author_email='ningfei.li@gmail.com',
+    packages=find_packages(),
+    install_requires=['requests'],
+)
